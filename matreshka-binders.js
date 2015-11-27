f2eb3b978cb30616e4138178426b2247a4b6f7c1
DenyZhirkov <denyzhirkov@yandex.ru>
2015-11-27 14:50:41 +0300

    array or object

--- matreshka-binders.js
+++ matreshka-binders.js
@@ -1,39 +1,36 @@
+/*
+Matreshka.js custom binders
+created by denyzhirkov@yandex.ru @dolphin4ik
+v 1.1
+*/
+
 ;(function (b) {
 if(b){
 
 //htmlSwitch(['first', 'second','third']) - Swithes between text values
+//htmlSwitch({'one':'super-text','two':'ultra-text'}) - Swithes between text
 b.htmlSwitch = function(arr){
 	return {
 		setValue: function(v){
 			this.innerHTML = arr[v] || '';
-		},
-		getValue: function(){
-			return arr.indexOf(this.innerHTML);
 		}
 	}
 };
 
 //cssSwitch(['first', 'second','third']) - Swithes between classes
+//cssSwitch({'one':'super-class','two':'ultra-class'}) - Swithes between classes
 b.cssSwitch = function(arr){	
 	function clear(el){
-		for (var i = arr.length - 1; i >= 0; i--) {
-				el.classList.remove(arr[i]);
-			};
+		for(var i in arr){
+			el.classList.remove(arr[i]);
+		}
 	};
 	return {
 		setValue: function(v){
 			clear(this);
-			if(v!==undefined && v!=-1 && arr[v]){
+			if(v!==undefined && arr[v]){
 				this.classList.add(arr[v]);
 			}
-		},
-		getValue: function(){
-			for (var i = this.classList.length - 1; i >= 0; i--) {
-				if(arr.indexOf(this.classList[i])){
-					return arr.indexOf(this.classList[i]);
-				}
-			};
-			return -1;
 		}
 	}
 };
